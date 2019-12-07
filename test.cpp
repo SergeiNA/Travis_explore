8754c941d100eceb2d324fed823f4ced6e8723d8
SergeiNA <nikseas@yandex.ru>
2019-12-07 16:31:48 +0300

    add simple prj

---
+++ test.cpp
@@ -0,0 +1,12 @@
+#include <map>
+#include <string_view>
+#include <string>
+
+
+int main(){
+    std::map<std::string,std::pair<std::string,std::string>> tmap;
+    tmap.try_emplace("one","struct","one");
+    auto tp1 = tmap["one"];
+    std::string_view sv = tp1.first;
+    return 0;
+}
